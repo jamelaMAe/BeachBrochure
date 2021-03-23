44deccc679d69484df335eb727efe9766b32b1e7
KGCM28 <78344641+KGCM28@users.noreply.github.com>
2021-03-23 19:50:36 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,301 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput} from 'react-native';
+
+
 
 export default function App() {
   return (
+
+  
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+   
+
+    <Text style ={{
+      height: 50, 
+      marginTop: 50,
+      fontSize: 45,
+      borderColor: '#87ceeb',
+       borderWidth: 30}}
+        >    Beach Brochure </Text>
+      
+      
+      
+      <Image style = {{width: 400, height: 350, borderColor: '#f0aac9'}}source = {{
+        uri: "https://qtxasset.com/styles/breakpoint_xl_880px_w/s3/Hotel%20Management-1508949476/mercuremaldiveskoodooresortmaldivesexterior.jpg?DEzB9TvzWN1QBDyaQFjxoiNGbAAXEis5&itok=V6RxrUEZ"}}></Image>
+      
+
+      
     </View>
+    <Text style ={{height: 15, 
+      borderColor: '#87ceeb',
+      fontSize: 25, 
+      fontStyle: 'italic',
+      
+      borderWidth: 20}} 
+      >                    Maldives </Text>
+
+
+      <TextInput style = {{
+        height: 15, 
+        borderColor:'#87ceeb', 
+        borderWidth: 1}} 
+        placeholder = "Maldives" 
+        value ="Rate 1 -10"/>
+
+        
+
+
+
+<View style={styles.container}>
+
+
+  
+  <Image style = {{width: 400, height: 350}}source = {{
+    uri: "https://miro.medium.com/max/700/1*EBoSBj8FZG-NC8EhtgmbxA.jpeg"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+      borderColor: '#87ceeb',
+      fontSize: 25, 
+      fontStyle: 'italic',
+      
+      borderWidth: 20}} 
+      >                     Boracay </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "Boracay" 
+    value ="Rate 1 -10"/>
+
+
+
+<View style={styles.container}>
+
+<Image style = {{width: 400, height: 250}}source = {{
+    uri: "https://www.lakwatsero.com/wp-content/uploads/2018/07/Lakawon-768x515.jpg"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+      borderColor: '#87ceeb',
+      fontSize: 25, 
+      fontStyle: 'italic',
+      
+      borderWidth: 20}} 
+      >                     Lakawon </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "Lakawon" 
+    value ="Rate 1 -10"/>
+
+
+
+<View style={styles.container}>
+
+<Image style = {{width: 400, height: 250}}source = {{
+    uri: "https://www.planetware.com/wpimages/2019/08/best-beaches-in-the-world-camps-bay-beach-cape-town-south-africa.jpg"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+      borderColor: '#87ceeb',
+      fontSize: 25, 
+      fontStyle: 'italic',
+      
+      borderWidth: 20}} 
+      >            Camps Bay Beach </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "Camps Bay Beach" 
+    value ="Rate 1 -10"/>
+
+
+
+
+<View style={styles.container}>
+
+<Image style = {{width: 400, height: 250}}source = {{
+    uri: "https://www.planetware.com/wpimages/2019/08/best-beaches-in-the-world-railay-beach-thailand.jpg"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+      borderColor: '#87ceeb',
+      fontSize: 25, 
+      fontStyle: 'italic',
+      
+      borderWidth: 20}} 
+      >                 Railay Beach </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "Railay Beach" 
+    value ="Rate 1 -10"/>
+
+
+
+
+    
+<View style={styles.container}>
+
+<Image style = {{width: 400, height: 250}}source = {{
+    uri: "https://www.planetware.com/wpimages/2019/08/best-beaches-in-the-world-whitehaven-beach-australia.jpg"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+      borderColor: '#87ceeb',
+      fontSize: 25, 
+      fontStyle: 'italic',
+      
+      borderWidth: 20}} 
+      >             Whitehaven Beach </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "Whitehaven Beach" 
+    value ="Rate 1 -10"/>
+
+
+
+<View style={styles.container}>
+
+<Image style = {{width: 400, height: 250}}source = {{
+    uri: "https://www.planetware.com/wpimages/2019/08/best-beaches-in-the-world-shipwreck-beach-zakynthos-greece.jpg"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+      borderColor: '#87ceeb',
+      fontSize: 25, 
+      fontStyle: 'italic',
+      
+      borderWidth: 20}} 
+      >            Shipwreck Beach </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "Shipwreck Beach" 
+    value ="Rate 1 -10"/>
+
+
+
+
+<View style={styles.container}>
+
+<Image style = {{width: 400, height: 250}}source = {{
+    uri: "https://www.planetware.com/wpimages/2019/08/best-beaches-in-the-world-pink-sands-beach-harbour-island-bahamas.jpg"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+      borderColor: '#87ceeb',
+      fontSize: 25, 
+      fontStyle: 'italic',
+      
+      borderWidth: 20}} 
+      >            Pink Sands Beach </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "Pink Sands Beach" 
+    value ="Rate 1 -10"/>
+
+
+
+
+<View style={styles.container}>
+
+<Image style = {{width: 400, height: 250}}source = {{
+    uri: "https://www.planetware.com/wpimages/2019/08/best-beaches-in-the-world-tulum-mexico.jpg"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+      borderColor: '#87ceeb',
+      fontSize: 25, 
+      fontStyle: 'italic',
+      
+      borderWidth: 20}} 
+      >                       Tulum </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "Tulum" 
+    value ="Rate 1 -10"/>
+
+
+
+
+<View style={styles.container}>
+
+<Image style = {{width: 400, height: 250}}source = {{
+    uri: "https://www.planetware.com/wpimages/2019/08/best-beaches-in-the-world-tulum-mexico.jpg"}}></Image>
+  
+
+  
+</View>
+<Text style ={{height: 15, 
+      borderColor: '#87ceeb',
+      fontSize: 25, 
+      fontStyle: 'italic',
+      
+      borderWidth: 20}} 
+      >                       La Jolla </Text>
+
+  <TextInput style = {{
+    height: 15, 
+    borderColor:'#87ceeb', 
+    borderWidth: 1}} 
+    placeholder = "La Jolla" 
+    value ="Rate 1 -10"/>
+
+
+
+
+
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
+
+    
   },
 });
